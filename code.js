ab0aed93eb880a48fdf5cf94aef98d2b325f5dee
abripourtous <marc@abripourtous.fr>
2024-10-18 15:08:06 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,23 @@
 
+var device="desktop"
+
+function main()
+{
+ alert(device)
+}
+
 function init()
 {
- alert("ok")
+ const agent=navigator.userAgent.toLowerCase()
+
+ if(agent.includes("ipad"))
+  device="desktop"
+ else if(agent.includes("iphone"))
+  device="mobile"
+ else if(agent.includes("android"))
+  device="mobile"
+
+main()
 }
 
 addEventListener('DOMContentLoaded',init);
-
